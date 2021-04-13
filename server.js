dc025938052dc912ce0b6b199c2d11dc68c303a7
Olegmail.com <oleglambin@g>
2021-04-13 20:52:14 +0300

    fix port

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(8080, () => {
-  console.log("JSON Server is running!");
+server.listen(process.env.PORT || 8080, () => {
+  console.log("JSON Server is running! port:", process.env.PORT);
 });
