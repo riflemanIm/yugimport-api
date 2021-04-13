8ded7cb3d57249d2fd74d1c663dedb920bcdfc81
Olegmail.com <oleglambin@g>
2021-04-13 20:41:18 +0300

    8080

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(6969, () => {
+server.listen(8080, () => {
   console.log("JSON Server is running!");
 });
